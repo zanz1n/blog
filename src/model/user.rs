b002c58ab144c776112e52bfda7a1f5f04e758ec
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-05 16:00:34 -0300

    adding user model

---
+++ src/model/user.rs
@@ -0,0 +1,56 @@
+use actix_web::{body::BoxBody, HttpResponse};
+use sea_orm::prelude::*;
+use serde::{Deserialize, Serialize};
+
+use crate::utils::http::DataBody;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "users", schema_name = "public")]
+pub struct Model {
+    #[sea_orm(primary_key, column_type = "String(Some(18))")]
+    pub id: String,
+    #[sea_orm(column_name = "createdAt")]
+    pub created_at: DateTime,
+    #[sea_orm(column_name = "updatedAt")]
+    pub updated_at: DateTime,
+    #[sea_orm(column_type = "String(Some(64))", unique, indexed)]
+    pub email: String,
+    #[sea_orm(column_type = "String(Some(24))", unique, indexed)]
+    pub username: String,
+    #[sea_orm(column_type = "String(Some(255))")]
+    pub password: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ApiUser {
+    pub id: String,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+    pub email: String,
+    pub username: String,
+}
+
+impl Model {
+    pub fn to_sendable(self) -> ApiUser {
+        ApiUser {
+            id: self.id,
+            created_at: self.created_at,
+            updated_at: self.updated_at,
+            email: self.email,
+            username: self.username,
+        }
+    }
+}
+
+impl actix_web::Responder for ApiUser {
+    type Body = BoxBody;
+
+    fn respond_to(self, req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        DataBody::new(self, "Success").respond_to(req)
+    }
+}
