4c2ad7589f0a34d40c5817c31c4ac3a62fdc7343
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-07 02:07:35 -0300

    adding function to convert error types

--- src/utils/db.rs
+++ src/utils/db.rs
@@ -1,26 +1,41 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use sea_orm::prelude::DateTime;
+use sea_orm::{prelude::DateTime, DbErr};
+
+use crate::repository::user::UserError;
 
 const USER_ID_SIZE: usize = 18;
 const POST_ID_SIZE: usize = 24;
 
-#[inline(always)]
+#[inline]
 pub fn random_id(size: usize) -> String {
     nanoid::nanoid!(size)
 }
 
-#[inline(always)]
+#[inline]
 pub fn random_user_id() -> String {
     nanoid::nanoid!(USER_ID_SIZE)
 }
 
-#[inline(always)]
+#[inline]
 pub fn random_post_id() -> String {
     nanoid::nanoid!(POST_ID_SIZE)
 }
 
-#[inline(always)]
+#[inline]
+pub fn db_to_user_error(db_err: DbErr, expect: UserError) -> UserError {
+    log::info!(target: "database_user_errors", "{}", db_err.to_string());
+
+    match db_err {
+        DbErr::Exec(_) => expect,
+        DbErr::Type(_) => UserError::InvalidData,
+        DbErr::Query(_) => expect,
+        DbErr::RecordNotFound(_) => UserError::NotFound,
+        _ => UserError::InternalServerError,
+    }
+}
+
+#[inline]
 pub fn now_unix_i64() -> Option<i64> {
     match SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -33,7 +48,7 @@ pub fn now_unix_i64() -> Option<i64> {
     }
 }
 
-#[inline(always)]
+#[inline]
 pub fn timestamp_now() -> Option<DateTime> {
     let now_ms = now_unix_i64()?;
     DateTime::from_timestamp_millis(now_ms)
