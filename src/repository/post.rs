26a6be7243e71a340a7c117b2ff0645e6c58294b
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-15 04:34:59 -0300

    updating posts repository [ adding create and other updates ]

--- src/repository/post.rs
+++ src/repository/post.rs
@@ -1,15 +1,38 @@
-use sea_orm::{DatabaseConnection, DbErr, EntityTrait, QueryOrder, QuerySelect};
-
+use super::cache::CacheService;
 use crate::{
     error::ApiError,
-    model::{
-        post::{Column as PostColumn, Entity as PostEntity, Model as PostModel},
-        user::Entity as UserEntity,
-    },
-    utils::db::USER_ID_SIZE,
+    model::post::{ActiveModel, Column as PostColumn, Entity as PostEntity, Model as PostModel},
+    utils::db::{random_post_id, timestamp_now, USER_ID_SIZE},
 };
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter, QueryOrder,
+    QuerySelect, Set,
+};
+use serde::{Deserialize, Serialize};
 
-use super::cache::CacheService;
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CreatePostData {
+    slug: String,
+    content: String,
+    #[serde(rename = "thumbImage")]
+    thumb_image: Option<String>,
+}
+
+impl CreatePostData {
+    pub fn is_valid(&self) -> Option<ApiError> {
+        if let Some(thumb_image) = self.thumb_image.as_ref() {
+            if thumb_image.len() > 128 {
+                return Some(ApiError::InvalidPostThumbIdSize);
+            }
+        }
+
+        if self.slug.len() > 64 || self.slug.len() < 12 {
+            return Some(ApiError::InvalidPostSlugSize);
+        }
+
+        None
+    }
+}
 
 pub struct PostRepository {
     db: &'static DatabaseConnection,
@@ -17,15 +40,57 @@ pub struct PostRepository {
 }
 
 impl PostRepository {
+    pub fn new(db: &'static DatabaseConnection, cm: &'static CacheService) -> Self {
+        Self { db, cm }
+    }
+
+    pub async fn create(
+        &self,
+        user_id: String,
+        data: CreatePostData,
+    ) -> Result<PostModel, ApiError> {
+        if let Some(err) = data.is_valid() {
+            return Err(err);
+        }
+
+        let now = match timestamp_now() {
+            Some(v) => v,
+            None => {
+                log::warn!("Failed to get timestamp");
+                return Err(ApiError::InternalServerError);
+            }
+        };
+
+        let post = ActiveModel {
+            id: Set(random_post_id()),
+            slug: Set(data.slug),
+            thumb_image: Set(data.thumb_image),
+            content: Set(data.content),
+            created_at: Set(now),
+            updated_at: Set(now),
+            user_id: Set(user_id),
+        };
+
+        let post = post
+            .insert(self.db)
+            .await
+            .or_else(|_| Err(ApiError::InternalServerError))?;
+
+        Ok(post)
+    }
+
     pub async fn get_by_id(&self, id: String) -> Result<PostModel, ApiError> {
         let result = PostEntity::find_by_id(id)
-            .inner_join(UserEntity)
+            // .inner_join(UserEntity)
             .one(self.db)
             .await
             .or_else(|e| {
                 Err(match e {
                     DbErr::RecordNotFound(_) => ApiError::PostNotFound,
-                    _ => ApiError::InternalServerError,
+                    e => {
+                        log::error!(target: "database_user_errors", "{}", e);
+                        ApiError::InternalServerError
+                    }
                 })
             })?;
 
@@ -48,13 +113,17 @@ impl PostRepository {
         }
 
         let result = PostEntity::find()
+            .filter(PostColumn::UserId.eq(id))
             .order_by_desc(PostColumn::CreatedAt)
             .limit(limit)
             .all(self.db)
             .await
             .or_else(|err| match err {
                 DbErr::RecordNotFound(_) => Ok(vec![]),
-                _ => Err(ApiError::InternalServerError),
+                e => {
+                    log::error!(target: "database_user_errors", "{}", e);
+                    Err(ApiError::InternalServerError)
+                }
             })?;
 
         Ok(result)
@@ -82,7 +151,10 @@ impl PostRepository {
                 .await
                 .or_else(|err| match err {
                     DbErr::RecordNotFound(_) => Ok(vec![]),
-                    _ => Err(ApiError::InternalServerError),
+                    e => {
+                        log::error!(target: "database_user_errors", "{}", e);
+                        Err(ApiError::InternalServerError)
+                    }
                 })?;
 
             match serde_json::to_string(&result) {
@@ -92,7 +164,7 @@ impl PostRepository {
                         .set_ttl("post-recomendation".to_string(), v, 60)
                         .await
                         .or_else(|e| {
-                            log::error!("Failed to cache post recomendations {}", e);
+                            log::error!("Failed to cache post recomendations: {}", e);
                             Err(())
                         });
                 }
