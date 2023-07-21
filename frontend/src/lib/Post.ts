58cb36f038bcdb2ac7abb0a2398da01361b45659
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-21 02:33:52 -0300

    adding post types

---
+++ frontend/src/lib/Post.ts
@@ -0,0 +1,88 @@
+import { Err, Ok, type Result } from "ts-results";
+import { JsonUser, User } from "./User";
+import { ApiError } from "./errors";
+
+export interface JsonPost {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  slug: string;
+  content: string;
+  thumbImage: string | null;
+  userId: string | null;
+  user: JsonUser | null;
+}
+
+export function isValidApiPost(payload: unknown): payload is JsonPost {
+  return true;
+}
+
+export interface PartialPost {
+  userId: string;
+}
+
+export interface CompletePost {
+  user: User;
+}
+
+export class Post {
+  private constructor(
+    public id: string,
+    public createdAt: Date,
+    public updatedAt: Date,
+    public slug: string,
+    public content: string,
+    public thumbImage: string | null,
+    public user?: User,
+    public userId?: string,
+  ) { }
+
+  isPartial(): this is PartialPost {
+    if (this.userId) {
+      return true;
+    }
+    return false;
+  }
+
+  isComplete(): this is CompletePost {
+    if (this.user) {
+      return true;
+    }
+    return false;
+  }
+
+  static fromObject(json: unknown): Result<Post, ApiError> {
+    if (!isValidApiPost(json)) {
+      return Err(ApiError.InvalidPostData);
+    }
+
+    const instance = new this(
+      json.id,
+      new Date(json.createdAt),
+      new Date(json.updatedAt),
+      json.slug,
+      json.content,
+      json.thumbImage,
+    );
+
+    if (json.user) {
+      instance.user = User.fromObject(json.user).unwrap();
+    } else if (json.userId) {
+      instance.userId = json.userId;
+    }
+
+    return Ok(instance);
+  }
+
+  static fromJson(payload: string): Result<Post, ApiError> {
+    let parsed: unknown;
+
+    try {
+      parsed = JSON.parse(payload) as unknown;
+    } catch (e) {
+      return new Err(ApiError.InvalidBodyPayload);
+    }
+
+    return this.fromObject(parsed);
+  }
+}
