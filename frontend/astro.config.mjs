95428a6eba68eab78fd6a1cf430296c5498890e7
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-21 05:40:05 -0300

    removing svelte dependency

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/edge";
 
-import svelte from "@astrojs/svelte";
-
 export default defineConfig({
-  integrations: [svelte()],
   output: "server",
   adapter: vercel()
 });
