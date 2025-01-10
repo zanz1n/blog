365aa0850a74c439685842334a7617af76308e67
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-01-09 21:19:12 -0300

    add user dto tests

---
+++ internal/dto/user_test.go
@@ -0,0 +1,29 @@
+package dto_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zanz1n/blog/internal/dto"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func userData() dto.UserCreateData {
+	return dto.UserCreateData{
+		Email:    "johhdoe@example.com",
+		Nickname: "johhdoe",
+		Name:     "John Doe",
+		Password: "strongpassword",
+	}
+}
+
+func TestUserPasswordMatches(t *testing.T) {
+	assert := require.New(t)
+
+	data := userData()
+	user, err := dto.NewUser(data, dto.PermissionDefault, bcrypt.MinCost)
+	assert.NoError(err)
+
+	matches := user.PasswordMatches(data.Password)
+	assert.True(matches)
+}
