1c9dae50a587a4dd63b4b8d8c8218e3eeee25182
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-01-11 19:10:12 -0300

    update user repository tests

--- internal/repository/user_test.go
+++ internal/repository/user_test.go
@@ -2,132 +2,173 @@ package repository_test
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
+	"log/slog"
+	"math/rand/v2"
+	"sync"
 	"testing"
 	"time"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pressly/goose/v3"
-	"github.com/stretchr/testify/require"
+	assert "github.com/stretchr/testify/require"
 	"github.com/zanz1n/blog/internal/dto"
 	"github.com/zanz1n/blog/internal/repository"
 	"github.com/zanz1n/blog/migrations"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func inMemoryUserRepo(t *testing.T) (*repository.UserRepository, *sqlx.DB) {
-	assert := require.New(t)
+var onceGoose sync.Once
 
+func init() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(log.Writer(), &slog.HandlerOptions{
+		AddSource: false,
+		Level:     slog.LevelInfo,
+	})))
+}
+
+func inMemoryUserRepo(t *testing.T) *repository.UserRepository {
 	db, err := sqlx.Open("sqlite3", "file::memory:")
-	assert.NoError(err)
+	assert.NoError(t, err)
+
 	userRepo, err := repository.NewUserRepository(db)
-	assert.NoError(err)
+	assert.NoError(t, err)
 
-	err = goose.SetDialect("sqlite3")
-	assert.NoError(err)
+	onceGoose.Do(func() {
+		err = goose.SetDialect("sqlite3")
+		assert.NoError(t, err)
 
-	goose.SetBaseFS(migrations.EmbedMigrations)
-	goose.SetLogger(log.New(io.Discard, "", log.Flags()))
+		goose.SetBaseFS(migrations.EmbedMigrations)
+		goose.SetLogger(log.New(io.Discard, "", log.Flags()))
+	})
 
 	err = goose.Up(db.DB, "sqlite")
-	assert.NoError(err)
+	assert.NoError(t, err)
 
-	return userRepo, db
+	t.Cleanup(func() {
+		userRepo.Close()
+		db.Close()
+	})
+
+	return userRepo
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	return string(b)
 }
 
 func userData() dto.UserCreateData {
 	return dto.UserCreateData{
-		Email:    "johhdoe@example.com",
-		Nickname: "johhdoe",
-		Name:     "John Doe",
-		Password: "strongpassword",
+		Email:    fmt.Sprintf("%s@%s.com", randString(10), randString(20)),
+		Nickname: randString(30),
+		Name:     fmt.Sprintf("%s %s", randString(7), randString(10)),
+		Password: randString(10),
 	}
 }
 
 func TestUserCreate(t *testing.T) {
-	assert := require.New(t)
+	t.Parallel()
+	repo := inMemoryUserRepo(t)
 
-	repo, db := inMemoryUserRepo(t)
-	defer db.Close()
-	defer repo.Close()
+	t.Run("Inexistent", func(t *testing.T) {
+		// t.Parallel()
+		_, err := repo.GetById(context.Background(), dto.NewSnowflake())
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, repository.ErrUserNotFound)
+	})
 
-	_, err := repo.GetById(context.Background(), dto.NewSnowflake())
-	assert.Error(err)
-	assert.ErrorIs(err, repository.ErrUserNotFound)
-
-	data := userData()
-
-	user, err := dto.NewUser(data, dto.PermissionDefault, bcrypt.MinCost)
-	assert.NoError(err)
+	user, err := dto.NewUser(userData(), dto.PermissionDefault, bcrypt.MinCost)
+	assert.NoError(t, err)
 
 	err = repo.Create(context.Background(), user)
-	assert.NoError(err)
+	assert.NoError(t, err)
 
-	user2, err := repo.GetById(context.Background(), user.ID)
-	assert.NoError(err)
-	assert.Equal(user, user2)
+	t.Run("Duplicate", func(t *testing.T) {
+		// t.Parallel()
+		err := repo.Create(context.Background(), user)
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, repository.ErrUserAlreadyExists)
+	})
 
-	user2, err = repo.GetByEmail(context.Background(), user.Email)
-	assert.NoError(err)
-	assert.Equal(user, user2)
+	t.Run("Fetch", func(t *testing.T) {
+		// t.Parallel()
+		user2, err := repo.GetById(context.Background(), user.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, user, user2)
+
+		user2, err = repo.GetByEmail(context.Background(), user.Email)
+		assert.NoError(t, err)
+		assert.Equal(t, user, user2)
+	})
 }
 
 func TestUserUpdateName(t *testing.T) {
-	assert := require.New(t)
-
-	repo, db := inMemoryUserRepo(t)
-	defer db.Close()
-	defer repo.Close()
+	t.Parallel()
+	repo := inMemoryUserRepo(t)
 
 	user, err := dto.NewUser(userData(), dto.PermissionDefault, bcrypt.MinCost)
-	assert.NoError(err)
+	assert.NoError(t, err)
 
 	err = repo.Create(context.Background(), user)
-	assert.NoError(err)
+	assert.NoError(t, err)
 
 	time.Sleep(5 * time.Millisecond)
 
-	user2, err := repo.UpdateName(context.Background(), user.ID, "New Name")
-	assert.NoError(err)
+	t.Run("Update", func(t *testing.T) {
+		user2, err := repo.UpdateName(context.Background(), user.ID, "New Name")
+		assert.NoError(t, err)
 
-	user.Name = "New Name"
-	assert.Greater(user2.UpdatedAt.UnixMilli(), user.UpdatedAt.UnixMilli())
-	user.UpdatedAt = user2.UpdatedAt
+		user.Name = "New Name"
+		assert.Greater(t, user2.UpdatedAt.UnixMilli(), user.UpdatedAt.UnixMilli())
+		user.UpdatedAt = user2.UpdatedAt
 
-	assert.Equal(user, user2)
+		assert.Equal(t, user, user2)
+	})
 
-	user2, err = repo.GetById(context.Background(), user.ID)
-	assert.NoError(err)
-	assert.Equal(user, user2)
+	t.Run("Fetch", func(t *testing.T) {
+		user2, err := repo.GetById(context.Background(), user.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, user, user2)
+	})
 }
 
 func TestUserDelete(t *testing.T) {
-	assert := require.New(t)
+	t.Parallel()
+	repo := inMemoryUserRepo(t)
 
-	repo, db := inMemoryUserRepo(t)
-	defer db.Close()
-	defer repo.Close()
+	t.Run("Inexistent", func(t *testing.T) {
+		// t.Parallel()
+		_, err := repo.DeleteById(context.Background(), dto.NewSnowflake())
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, repository.ErrUserNotFound)
+	})
 
-	_, err := repo.DeleteById(context.Background(), dto.NewSnowflake())
-	assert.Error(err)
-	assert.ErrorIs(err, repository.ErrUserNotFound)
-
-	data := userData()
-
-	user, err := dto.NewUser(data, dto.PermissionDefault, bcrypt.MinCost)
-	assert.NoError(err)
+	user, err := dto.NewUser(userData(), dto.PermissionDefault, bcrypt.MinCost)
+	assert.NoError(t, err)
 
 	err = repo.Create(context.Background(), user)
-	assert.NoError(err)
+	assert.NoError(t, err)
 
-	user2, err := repo.DeleteById(context.Background(), user.ID)
-	assert.NoError(err)
+	t.Run("Delete", func(t *testing.T) {
+		user2, err := repo.DeleteById(context.Background(), user.ID)
+		assert.NoError(t, err)
 
-	assert.Equal(user, user2)
+		assert.Equal(t, user, user2)
+	})
 
-	_, err = repo.GetById(context.Background(), user.ID)
-	assert.Error(err)
-	assert.ErrorIs(err, repository.ErrUserNotFound)
+	t.Run("Fetch", func(t *testing.T) {
+		_, err = repo.GetById(context.Background(), user.ID)
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, repository.ErrUserNotFound)
+	})
 }
