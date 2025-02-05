6c8ad423dbd9285c4965d6451ca959d5f3afe23b
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-02-05 17:50:23 -0300

    update pagination dto

--- internal/dto/pagination.go
+++ internal/dto/pagination.go
@@ -1,6 +1,6 @@
 package dto
 
 type Pagination struct {
-	Limit  uint `json:"limit" schema:"limit,default:100" validate:"gte=1,lte=100"`
-	Offset uint `json:"offset" schema:"limit,default:0" validate:"lte=10000"`
+	Limit  int `json:"limit" schema:"limit,default:100" validate:"gte=1,lte=100"`
+	Offset int `json:"offset" schema:"limit,default:0" validate:"lte=10000"`
 }
