0c452cbb735ef46ac641bec4bc234d15626445ab
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-02-23 22:05:11 -0300

    add server package

---
+++ internal/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/zanz1n/blog/internal/repository"
+	"github.com/zanz1n/blog/internal/utils/xhttp"
+)
+
+type Server struct {
+	users *repository.UserRepository
+	auth  *repository.AuthRepository
+}
+
+func New(
+	users *repository.UserRepository,
+	auth *repository.AuthRepository,
+) *Server {
+	return &Server{
+		users: users,
+		auth:  auth,
+	}
+}
+
+func (s *Server) Wire(r chi.Router) {}
+
+func m(h xhttp.HandlerFunc) http.HandlerFunc {
+	return xhttp.ErrorMiddleware(h)
+}
