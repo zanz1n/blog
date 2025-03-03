e79b9df28038a77d9990a94b8ad7083757587ccf
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-03-03 01:57:11 -0300

    update theming script

--- web/src/entries/theming.ts
+++ web/src/entries/theming.ts
@@ -1,4 +1,20 @@
-import { getTheme, setTheme } from "@lib/theming";
+import { getTheme, setTheme, storeTheme, Theme } from "@lib/theming";
 
 const currtheme = getTheme();
 setTheme(currtheme);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const lightswitch = document.getElementById(
+        "lightswitch",
+    ) as HTMLInputElement | null;
+
+    if (lightswitch) {
+        lightswitch.checked = currtheme == Theme.Light;
+    }
+
+    lightswitch.addEventListener("change", () => {
+        const theme = lightswitch.checked ? Theme.Light : Theme.Dark;
+        setTheme(theme);
+        storeTheme(theme);
+    });
+});
