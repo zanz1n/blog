d0c0f9f645f4bd3d64d680f0a3e68af217823bc0
Izan Rodrigues <izanrodrigues999@gmail.com>
2025-04-13 11:29:40 -0300

    update repository utils: remove sqlite3 from lambda build

--- internal/repository/utils_lambda.go
+++ internal/repository/utils_lambda.go
@@ -1,22 +1,15 @@
-//go:build !lambda
-// +build !lambda
+//go:build lambda
+// +build lambda
 
 package repository
 
 import (
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/mattn/go-sqlite3"
 )
 
 func isUniqueConstraintViolation(err error) bool {
-	if sqliteErr, ok := err.(sqlite3.Error); ok {
-		if sqliteErr.Code == sqlite3.ErrConstraint {
-			return true
-		} else {
-			return false
-		}
-	} else if pgErr, ok := err.(*pgconn.PgError); ok {
+	if pgErr, ok := err.(*pgconn.PgError); ok {
 		if pgErr.Code == pgerrcode.UniqueViolation {
 			return true
 		} else {
